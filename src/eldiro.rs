4b9048639e706505ac1c30e91944a87541964f6c
Rareș Cosma <rares@getbetter.ro>
2020-11-12 18:29:13 +0100

    Implement Eldiro expression parser with whitespaces

---
+++ src/eldiro.rs
@@ -0,0 +1,194 @@
+#[derive(Debug, PartialEq)]
+pub struct Number(pub i32);
+
+impl Number {
+    pub fn new(s: &str) -> (&str, Self) {
+        let (s, number) = extract_digits(s);
+        (s, Self(number.parse().unwrap()))
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Op {
+    Add,
+    Sub,
+    Mul,
+    Div,
+}
+
+impl Op {
+    pub fn new(s: &str) -> (&str, Self) {
+        let (s, op) = extract_op(s);
+        (s, match op {
+            "+" => Self::Add,
+            "-" => Self::Sub,
+            "*" => Self::Mul,
+            "/" => Self::Div,
+            _ => panic!("bad operator"),
+        })
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Expr {
+    pub lhs: Number,
+    pub rhs: Number,
+    pub op: Op,
+}
+
+impl Expr {
+    pub fn new(s: &str) -> (&str, Self) {
+        let (s, lhs) = Number::new(s);
+        let (s, _) = extract_whitespace(s);
+
+        let (s, op) = Op::new(s);
+        let (s, _) = extract_whitespace(s);
+
+        let (s, rhs) = Number::new(s);
+
+        (s, Self { lhs, rhs, op })
+    }
+}
+
+fn extract_digits(s: &str) -> (&str, &str) {
+    let mut digits_end = 0;
+
+    for (idx, c) in s.char_indices() {
+        if c.is_ascii_digit() {
+            digits_end = idx + 1;
+        } else {
+            break;
+        }
+    }
+
+    (&s[digits_end..], &s[..digits_end])
+}
+
+fn extract_whitespace(s: &str) -> (&str, &str) {
+    let mut digits_end = 0;
+
+    for (idx, c) in s.char_indices() {
+        if c.is_whitespace() {
+            digits_end = idx + 1;
+        } else {
+            break;
+        }
+    }
+
+    (&s[digits_end..], &s[..digits_end])
+}
+
+fn extract_op(s: &str) -> (&str, &str) {
+    match &s[0..1] {
+        "+" | "-" | "*" | "/" => {}
+        _ => panic!("bad operator"),
+    }
+
+    (&s[1..], &s[0..1])
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_number() {
+        assert_eq!(Number::new("123"), ("", Number(123)));
+    }
+
+    #[test]
+    fn parse_add_op() {
+        assert_eq!(Op::new("+"), ("", Op::Add));
+    }
+
+    #[test]
+    fn parse_sub_op() {
+        assert_eq!(Op::new("-"), ("", Op::Sub));
+    }
+
+    #[test]
+    fn parse_mul_op() {
+        assert_eq!(Op::new("*"), ("", Op::Mul));
+    }
+
+    #[test]
+    fn parse_div_op() {
+        assert_eq!(Op::new("/"), ("", Op::Div));
+    }
+
+    #[test]
+    fn parse_one_plus_two() {
+        assert_eq!(
+            Expr::new("1+2"),
+            (
+                "",
+                Expr {
+                    lhs: Number(1),
+                    rhs: Number(2),
+                    op: Op::Add,
+                }
+            ),
+        );
+    }
+
+    #[test]
+    fn parse_expr_with_whitespace() {
+        assert_eq!(
+            Expr::new("2 * 2"),
+            (
+                "",
+                Expr {
+                    lhs: Number(2),
+                    rhs: Number(2),
+                    op: Op::Mul,
+                },
+            ),
+        );
+    }
+
+    #[test]
+    fn extract_one_digit() {
+        assert_eq!(extract_digits("1+2"), ("+2", "1"));
+    }
+
+    #[test]
+    fn extract_multiple_digits() {
+        assert_eq!(extract_digits("10-20"), ("-20", "10"));
+    }
+
+    #[test]
+    fn do_not_extract_anything_from_empty_input() {
+        assert_eq!(extract_digits(""), ("", ""));
+    }
+
+    #[test]
+    fn extract_digits_with_no_remainder() {
+        assert_eq!(extract_digits("100"), ("", "100"));
+    }
+
+    #[test]
+    fn extract_plus() {
+        assert_eq!(extract_op("+2"), ("2", "+"));
+    }
+
+    #[test]
+    fn extract_minus() {
+        assert_eq!(extract_op("-10"), ("10", "-"));
+    }
+
+    #[test]
+    fn extract_star() {
+        assert_eq!(extract_op("*3"), ("3", "*"));
+    }
+
+    #[test]
+    fn extract_slash() {
+        assert_eq!(extract_op("/4"), ("4", "/"));
+    }
+
+    #[test]
+    fn extract_spaces() {
+        assert_eq!(extract_whitespace("    1"), ("1", "    "));
+    }
+}
