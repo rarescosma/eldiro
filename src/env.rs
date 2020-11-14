37889459b1770c216a43a9c9b3d9af123a00a716
Rareș Cosma <rares@getbetter.ro>
2020-11-14 21:00:02 +0100

    Allow for environment chaining

--- src/env.rs
+++ src/env.rs
@@ -2,22 +2,36 @@ use crate::val::Val;
 use std::collections::HashMap;
 
 #[derive(Debug, PartialEq, Default)]
-pub(crate) struct Env {
+pub(crate) struct Env<'parent> {
     bindings: HashMap<String, Val>,
+    parent: Option<&'parent Self>,
 }
 
-impl Env {
+impl<'parent> Env<'parent> {
+    pub(crate) fn create_child(&'parent self) -> Self {
+        Self {
+            bindings: HashMap::new(),
+            parent: Some(self),
+        }
+    }
+
     pub(crate) fn store_binding(&mut self, name: String, val: Val) {
         self.bindings.insert(name, val);
     }
 
     pub(crate) fn get_binding_value(&self, name: &str) -> Result<Val, String> {
-        self.bindings
-            .get(name)
-            .cloned()
+        self.chain_lookup(name)
             .ok_or_else(|| format!(
                 "binding with name '{}' does not exist",
                 name,
             ))
     }
+
+    fn chain_lookup(&self, name: &str) -> Option<Val> {
+        self.bindings.get(name).cloned().or_else(|| {
+            self.parent.and_then(|parent| parent.chain_lookup(name))
+        })
+    }
 }
+
+
--- src/expr/block.rs
+++ src/expr/block.rs
@@ -32,11 +32,11 @@ impl Block {
             return Ok(Val::Unit)
         }
 
-        let mut env = Env::default(); // wop wop
+        let mut child_env = env.create_child();
         let mut res = Ok(Val::Unit);
 
         for stmt in &self.stmts {
-            res = stmt.eval(&mut env);
+            res = stmt.eval(&mut child_env);
         }
         res
     }
@@ -44,10 +44,9 @@ impl Block {
 
 #[cfg(test)]
 mod tests {
-    use super::super::{Expr, Number};
+    use super::super::{Expr, Number, BindingUsage, Op};
     use super::*;
     use crate::binding_def::BindingDef;
-    use crate::expr::binding_usage::BindingUsage;
 
 
     #[test]
@@ -135,6 +134,70 @@ mod tests {
                 ],
             }.eval(&Env::default()),
             Ok(Val::Number(1)),
+        );
+    }
+
+    #[test]
+    fn eval_block_with_multiple_binding_defs() {
+        assert_eq!(
+            Block {
+                stmts: vec![
+                    Stmt::BindingDef(BindingDef {
+                        name: "foo".to_string(),
+                        val: Expr::Number(Number(5)),
+                    }),
+                    Stmt::BindingDef(BindingDef {
+                        name: "bar".to_string(),
+                        val: Expr::Number(Number(4)),
+                    }),
+                    Stmt::BindingDef(BindingDef {
+                        name: "baz".to_string(),
+                        val: Expr::Number(Number(3)),
+                    }),
+                ],
+            }.eval(&Env::default()),
+            Ok(Val::Unit),
+        );
+    }
+
+    #[test]
+    fn eval_block_with_multiple_exprs() {
+        assert_eq!(
+            Block {
+                stmts: vec![
+                    Stmt::Expr(Expr::Number(Number(100))),
+                    Stmt::Expr(Expr::Number(Number(30))),
+                    Stmt::Expr(Expr::Operation {
+                        lhs: Number(10),
+                        rhs: Number(7),
+                        op: Op::Sub,
+                    }),
+                ],
+            }.eval(&Env::default()),
+            Ok(Val::Number(3)),
+        );
+    }
+
+    #[test]
+    fn eval_block_using_bindings_from_parent_env() {
+        let mut env = Env::default();
+        env.store_binding("foo".to_string(), Val::Number(2));
+
+        assert_eq!(
+            Block {
+                stmts: vec![
+                    Stmt::BindingDef(BindingDef {
+                        name: "baz".to_string(),
+                        val: Expr::BindingUsage(BindingUsage {
+                            name: "foo".to_string(),
+                        }),
+                    }),
+                    Stmt::Expr(Expr::BindingUsage(BindingUsage {
+                        name: "baz".to_string(),
+                    })),
+                ],
+            }.eval(&env),
+            Ok(Val::Number(2)),
         );
     }
 }
