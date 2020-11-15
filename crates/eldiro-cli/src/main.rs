cd81dafd18bd67b9526bd2919e9b817ebbe78d84
Rareș Cosma <rares@getbetter.ro>
2020-11-15 10:57:56 +0100

    Fancy REPL with readline support

--- crates/eldiro-cli/src/main.rs
+++ crates/eldiro-cli/src/main.rs
@@ -1,28 +1,45 @@
-use std::io::{self, Write};
+use rustyline::Editor;
+use rustyline::error::ReadlineError;
 
-fn main() -> io::Result<()> {
-    let stdin = io::stdin();
-    let mut stdout = io::stdout();
-    let mut stderr = io::stderr();
+fn main() {
+    let mut rl = Editor::<()>::new();
+    let history_file = ".eldiro_history";
+    if rl.load_history(history_file).is_err() {
+        println!("No previous history.");
+    }
 
-    let mut input = String::new();
     let mut env = eldiro::Env::default();
 
     loop {
-        write!(stdout, "→ ")?;
-        stdout.flush()?;
-
-        stdin.read_line(&mut input)?;
-
-        match run(input.trim(), &mut env) {
-            Ok(Some(val)) => writeln!(stdout, "{}", val)?,
-            Ok(None) => {}
-            Err(msg) => writeln!(stderr, "{}", msg)?,
+        let readline = rl.readline("→ ");
+        match readline {
+            Ok(line) => {
+                let line = line.as_str().trim();
+                rl.add_history_entry(line);
+                match run(line, &mut env) {
+                    Ok(Some(val)) => println!("{}", val),
+                    Ok(None) => {}
+                    Err(msg) => println!("{}", msg),
+                }
+            },
+            Err(ReadlineError::Interrupted) => {
+                println!("CTRL-C");
+                break
+            },
+            Err(ReadlineError::Eof) => {
+                println!("CTRL-D");
+                break
+            },
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break
+            }
         }
-
-        input.clear();
     }
+    rl.save_history(history_file).unwrap();
 }
+
+
 
 fn run(input: &str, env: &mut eldiro::Env) -> Result<Option<eldiro::Val>, String> {
     let parse = eldiro::parse(input)
